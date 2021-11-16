9f4c53eea4f0aabc30c674e2962e27b1c3f7dea1
Hanna <anna.shliakhova@raketech.com>
2021-11-17 01:06:51 +0200

    initial markup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/general.scss';
+
+const App = () => (
+	<div>
+		<header className="appHeader">
+			<h1>To-do application</h1>
+		</header>
+		<div className="container">
+			App gonna be here
+		</div>
+	</div>
+);
+
+export default App;
