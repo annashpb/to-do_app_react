68541c7953b32fca390bdd8a728ec14c7ee2ec63
unknown <anna.sh.pb@gmail.com>
2021-12-12 02:05:25 +0200

    checklist items editing handlers added

--- src/components/AddItemForm/Checklist/index.jsx
+++ src/components/AddItemForm/Checklist/index.jsx
@@ -2,9 +2,14 @@ import React, { useState, useRef } from 'react';
 import styles from './styles.module.scss';
 
 const Checklist = () => {
-	const [addChecklistItem, setAddChecklistItem] = useState(false);
 	const [checklist, setChecklist] = useState([]);
+
+	const [addChecklistItem, setAddChecklistItem] = useState(false);
 	const newChecklistItem = useRef();
+
+	const [itemEditing, setItemEditing] = useState(null);
+	const [itemOnEditText, setItemOnEditText] = useState(null);
+	const itemOnEdit = useRef();
 
 	const newItemHandle = () => {
 		setAddChecklistItem(true);
@@ -28,6 +33,34 @@ const Checklist = () => {
 		setAddChecklistItem(false);
 	}
 
+	const startEditItemHandle = i => {
+		setItemEditing(i);
+		setItemOnEditText(checklist[i]);
+	}
+
+	const editItemTextHandle = ({ target }) => {
+		setItemOnEditText(target.value);
+	}
+
+	const approveChangesHandle = i => {
+		const CL = [...checklist];
+		CL.splice(i, 1, itemOnEditText);
+		setChecklist(CL);
+		setItemEditing(null);
+		setItemOnEditText(null);
+	}
+
+	const editEnterPressHandle = (e, i) => {
+		if (e.key === 'Enter') {
+			approveChangesHandle(i);
+		}
+	}
+
+	const cancelChangesHandle = () => {
+		setItemEditing(null);
+		setItemOnEditText(null);
+	}
+
 	const removeItemHandle = i => {
 		const CL = [...checklist];
 		CL.splice(i, 1);
@@ -39,28 +72,62 @@ const Checklist = () => {
 			{checklist.length > 0 && (
 				<ul style={{ marginTop: '0' }}>
 					{checklist.map((item, i) => (
-						<li className={styles.checklistItem}>
-							<span>{item}</span>
-							<button
-								type="button"
-								name="edit-checklist-item"
-								title="Edit"
-								// onClick={() => editChecklistItemHandle(i)}
-								className={`${styles.itemBtn} ${styles.edit}`}
-							>
-								&#9998;
-							</button>
-							<button
-								type="button"
-								name="remove-checklist-item"
-								title="Remove item"
-								onClick={() => removeItemHandle(i)}
-								className={`${styles.itemBtn} ${styles.remove}`}
-							>
-								&#10007;
-							</button>
-						</li>
-					))}
+						i === itemEditing
+							? (
+								<li className={styles.newItemWrapper}>
+									<input
+										type="text"
+										value={itemOnEditText}
+										name="checklist-item"
+										autoFocus={true}
+										ref={itemOnEdit}
+										onChange={editItemTextHandle}
+										onKeyPress={e => editEnterPressHandle(e, i)}
+									/>
+									<button
+										type="button"
+										name="add-checklist-item"
+										title="Add item"
+										onClick={() => approveChangesHandle(i)}
+										className={`${styles.newItemBtn} ${styles.add}`}
+									>
+										&#10003;
+									</button>
+									<button
+										type="button"
+										name="cancel-checklist-item"
+										title="Cancel"
+										onClick={cancelChangesHandle}
+										className={`${styles.newItemBtn} ${styles.cancel}`}
+									>
+										&#10007;
+									</button>
+								</li>
+							) : (
+								<li className={styles.checklistItem}>
+									<span>{item}</span>
+									<button
+										type="button"
+										name="edit-checklist-item"
+										title="Edit"
+										onClick={() => startEditItemHandle(i)}
+										className={`${styles.itemBtn} ${styles.edit}`}
+									>
+										&#9998;
+									</button>
+									<button
+										type="button"
+										name="remove-checklist-item"
+										title="Remove item"
+										onClick={() => removeItemHandle(i)}
+										className={`${styles.itemBtn} ${styles.remove}`}
+									>
+										&#10007;
+									</button>
+								</li>
+							)
+						)
+					)}
 				</ul>
 			)}
 			{addChecklistItem && (
